af64a15136b6762e126c1e469a452c21d0258e63
saif <saifalis@axioned.com>
2022-09-27 11:39:19 +0530

    pokedex function half completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
-/* Author: 
+// Pokedex apiCall function
+var pokemonCards = document.querySelector('.pokemon-cards');
 
-*/
+var counter = 150;
 
+getPokemon();
 
+function getPokemon() {
+  for (let i = 1; i <= counter; i++) {
+    fetchPokemons(i);
+  }
+}
 
+function fetchPokemons(i) {
+  fetch('https://pokeapi.co/api/v2/pokemon/')
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    showPokemons(data.results);
+  })
+}
 
+function showPokemons(cards) {
+  console.log(cards);
+}
 
 
 
